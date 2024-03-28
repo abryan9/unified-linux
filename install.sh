4f78ddde6b177dfe805cf13c78cd1c4654514abc
abryan9 <144745136+abryan9@users.noreply.github.com>
2024-03-28 17:43:57 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,1 @@
-cd /
-git clone https://github.com/abryan9/unified-linux.git
-cd /unified-linux/src
-sudo chmod +x ./setup.sh && sudo ./setup.sh
+cd / && git clone https://github.com/abryan9/unified-linux.git && cd /unified-linux/src && sudo chmod +x ./setup.sh && sudo ./setup.sh 
