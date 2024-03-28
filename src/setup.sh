9634edb2f0095525f3c346da596e7219ff612c3b
abryan9 <144745136+abryan9@users.noreply.github.com>
2024-03-28 17:44:52 -0600

    Update setup.sh

--- src/setup.sh
+++ src/setup.sh
@@ -1,1 +1,1 @@
-git config --global --add safe.directory /unified-linux && sudo chmod +x ./on_init.sh  && sudo ./on_init.sh && cd ~/Downloads &&npm install electron --safe-dev 
+git config --global --add safe.directory /unified-linux && sudo chmod +x ./on_init.sh  && sudo ./on_init.sh && cd ~/Downloads && npm install electron --safe-dev 
